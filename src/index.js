d5a7b5ca0cc11f799adaea76798b39056a34cf88
Ronaldo B Ferreyra <ronaldoferreyra@yahoo.com.br>
2020-04-27 22:17:10 -0300

    Tipos de parâmetros

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ const express = require('express');
 
 const app = express();
 
+//para interpretar o conteúdo das entradas feitas em POST/PUT utilizando o BODY
+//deve atribuir o metodo abaixo, para recuperar essas entradas em JSON
+app.use(express.json());
+
 app.get('/', (request, response) => {
     return response.json({ message: 'Hello world' });
 });
 
 app.get('/projects', (request, response) => {
+    const { title, owner } = request.query; // query params
+
+    console.log(title);
+    console.log(owner);
+
     return response.json([
         'Projeto 01',
         'Projeto 02',
@@ -14,6 +23,11 @@ app.get('/projects', (request, response) => {
 });
 
 app.post('/projects', (request, response) => {
+    const { title, owner } = request.body; // conteúdo das entradas usando o BODY
+
+    console.log(title);
+    console.log(owner);
+
     return response.json([
         'Projeto 01',
         'Projeto 02',
@@ -22,6 +36,10 @@ app.post('/projects', (request, response) => {
 });
 
 app.put('/projects/:id', (request, response) => {
+    const { id } = request.params; // route params
+
+    console.log(id);
+
     return response.json([
         'Projeto 04',
         'Projeto 02',
@@ -30,6 +48,10 @@ app.put('/projects/:id', (request, response) => {
 });
 
 app.delete('/projects/:id', (request, response) => {
+    const { id } = request.params; // route params
+
+    console.log(id);
+
     return response.json([
         'Projeto 02',
         'Projeto 03',

26cfde83e0cc83f14d1d2e64956118ef5d7bb621
Ronaldo B Ferreyra <ronaldoferreyra@yahoo.com.br>
2020-04-27 21:56:56 -0300

    Incluir recursos de projetos com todos os métodos

--- src/index.js
+++ src/index.js
@@ -6,6 +6,36 @@ app.get('/', (request, response) => {
     return response.json({ message: 'Hello world' });
 });
 
+app.get('/projects', (request, response) => {
+    return response.json([
+        'Projeto 01',
+        'Projeto 02',
+    ]);
+});
+
+app.post('/projects', (request, response) => {
+    return response.json([
+        'Projeto 01',
+        'Projeto 02',
+        'Projeto 03',
+    ]);
+});
+
+app.put('/projects/:id', (request, response) => {
+    return response.json([
+        'Projeto 04',
+        'Projeto 02',
+        'Projeto 03',
+    ]);
+});
+
+app.delete('/projects/:id', (request, response) => {
+    return response.json([
+        'Projeto 02',
+        'Projeto 03',
+    ]);
+});
+
 app.listen(3333, () => {
     console.log('Backend started!');
 });
